4127e4e40bc273ec7eade442ce516e8a5e0ce97f
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-05-17 18:19:10 -0500

    feat(fake-api): update.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   // In real lifecycles, this file should not be part of version control
   production: true,
   // Server Base URL for REST API
-  serverBaseUrl: 'https://682916006075e87073a5b424.mockapi.io/api/v1',
+  serverBaseUrl: 'http://localhost:3000/api/v1',
   coursesEndpointPath: '/courses',
 };
 
